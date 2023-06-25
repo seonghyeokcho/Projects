60e807f4d04a25ed418a4b8a4d154fe7eab40803
seonghyeokcho <whtjdgur77@gmail.com>
2023-06-26 00:12:02 +0900

    Update colorization.py

--- Colorization/colorization.py
+++ Colorization/colorization.py
@@ -1,6 +1,3 @@
-# Script is based on https://github.com/richzhang/colorization/blob/master/colorization/colorize.py
-# To download the caffemodel and the prototxt, see: https://github.com/richzhang/colorization/tree/master/colorization/models
-# To download pts_in_hull.npy, see: https://github.com/richzhang/colorization/blob/master/colorization/resources/pts_in_hull.npy
 import numpy as np
 import cv2 as cv
 
@@ -42,24 +39,26 @@ if __name__ == '__main__':
         img_rgb = (frame[:,:,[2, 1, 0]] * 1.0 / 255).astype(np.float32)
 
         img_lab = cv.cvtColor(img_rgb, cv.COLOR_RGB2Lab)
-        img_l = img_lab[:,:,0] # pull out L channel
-        (H_orig,W_orig) = img_rgb.shape[:2] # original image size
+        img_l = img_lab[:,:,0] # L 채널 따로 꺼내기
+        (H_orig,W_orig) = img_rgb.shape[:2] # 원본 이미지 크기
 
-        # resize image to network input size
-        img_rs = cv.resize(img_rgb, (W_in, H_in)) # resize image to network input size
+        # 네트워크 입력 사이즈에 맞게 이미지 크기를 재구성
+        img_rs = cv.resize(img_rgb, (W_in, H_in))
         img_lab_rs = cv.cvtColor(img_rs, cv.COLOR_RGB2Lab)
         img_l_rs = img_lab_rs[:,:,0]
-        img_l_rs -= 50 # subtract 50 for mean-centering
+        img_l_rs -= 50 # 평균 중심화를 위해 전체적으로 50을 빼준다.
 
         net.setInput(cv.dnn.blobFromImage(img_l_rs))
-        ab_dec = net.forward()[0,:,:,:].transpose((1,2,0)) # this is our result
+        ab_dec = net.forward()[0,:,:,:].transpose((1,2,0)) # 결과물
 
         (H_out,W_out) = ab_dec.shape[:2]
         ab_dec_us = cv.resize(ab_dec, (W_orig, H_orig))
-        img_lab_out = np.concatenate((img_l[:,:,np.newaxis],ab_dec_us),axis=2) # concatenate with original image L
+        img_lab_out = np.concatenate((img_l[:,:,np.newaxis],ab_dec_us),axis=2) # 원본 이미지의 L 채널과 연결
         img_bgr_out = np.clip(cv.cvtColor(img_lab_out, cv.COLOR_Lab2BGR), 0, 1)
 
         frame = cv.resize(frame, imshowSize)
+        
+        # 화면에 출력
         cv.imshow('origin', frame)
         cv.imshow('gray', cv.cvtColor(frame, cv.COLOR_RGB2GRAY))
         cv.imshow('colorized', cv.resize(img_bgr_out, imshowSize))
